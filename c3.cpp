f65ac25708dce83afbb369556a88621f2f4db7bd
Saanvi01Mohapatra <saanvimohapatra05@gmail.com>
2024-08-28 01:02:48 +0530

    Add files via upload

---
+++ c3.cpp
@@ -0,0 +1,26 @@
+// Saanvi
+// Prn: 23070123110
+// Experiment 11:
+
+#include <iostream>
+using namespace std;
+
+class Cube {
+    private:
+        int height = 5;  // Height
+        int width = 9;   // Width 
+        int length = 6;  // Length 
+
+    public:
+         
+        double volume() {
+            // Calculate the volume and return it as a double
+            return static_cast<double>(height * width * length);
+        }
+};
+
+int main() {
+    Cube cube1;  
+    cout << "Volume: " << cube1.volume() << endl;
+    return 0;  
+}
